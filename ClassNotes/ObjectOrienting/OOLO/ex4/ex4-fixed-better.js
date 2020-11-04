78db8046067b474587a60e6876c0fc3ad786d6e1
Legz4Dayz <mitchlivingston25@gmail.com>
2020-11-03 20:15:41 -0600

    Edited prototype example number 3

--- ClassNotes/ObjectOrienting/OOLO/ex4/ex4-fixed-better.js
+++ ClassNotes/ObjectOrienting/OOLO/ex4/ex4-fixed-better.js
@@ -1,10 +1,10 @@
 var Widget = {
-	init: function(width,height){
+	init: function init(width,height){
 		this.width = width || 50;
 		this.height = height || 50;
 		this.$elem = null;
 	},
-	insert: function($where){
+	insert: function insert($where){
 		if (this.$elem) {
 			this.$elem.css({
 				width: this.width + "px",
@@ -16,23 +16,23 @@ var Widget = {
 
 var Button = Object.create(Widget);
 
-Button.setup = function(width,height,label){
+Button.setup = function setup(width,height,label){
 	// delegated call
 	this.init(width,height);
 	this.label = label || "Default";
 
 	this.$elem = $("<button>").text(this.label);
 };
-Button.build = function($where) {
+Button.build = function build($where) {
 	// delegated call
 	this.insert($where);
 	this.$elem.click(this.onClick.bind(this));
 };
-Button.onClick = function(evt) {
+Button.onClick = function event(evt) {
 	console.log("Button '" + this.label + "' clicked!");
 };
 
-$(document).ready(function(){
+$(function(){
 	var $body = $(document.body);
 
 	var btn1 = Object.create(Button);
@@ -43,4 +43,13 @@ $(document).ready(function(){
 
 	btn1.build($body);
 	btn2.build($body);
+
+	var $TestDiv = $("#Test");
+
+	var btn3 = Object.create(Button);
+	btn3.setup(100, 30, "Test button");
+	btn3.build($TestDiv);
+
+	console.log(btn3.__proto__);
+
 });
--- ClassNotes/ObjectOrienting/prototype3.js
+++ ClassNotes/ObjectOrienting/prototype3.js
@@ -3,12 +3,12 @@ function Foo(who){
 }
 
 Foo.prototype.identify = function(){
-    return "I am " + this.me;
+    console.log("I am " + this.me);
 }
 
 function Bar(who){
-    console.log(this);
-    console.log('^this');
+    // console.log(this);   Constructor
+    // console.log('^this');
     Foo.call(this, who);
 }
 
@@ -16,11 +16,29 @@ function Bar(who){
 Bar.prototype = Object.create(Foo.prototype);
 
 Bar.prototype.speak = function(){
-    console.log("Hello, " + this.identify() + ".");
+    console.log("Hello, I am Bar.");
 };
+
+Bar.prototype.identify = function(){
+    console.log("Bar Identity");
+}
+
+Bar.prototype.all = function(){
+    this.identify();
+    this.speak();
+    console.log('All Test^');
+}
 
 var b1 = new Bar("b1");
 var b2 = new Bar("b2");
 
-b1.speak();
-b2.speak();
+// b1.speak();
+// b1.identify();
+// b2.speak();
+// b2.identify()
+
+// var f1 = new Foo("f1");
+// // f1.speak(); not defined
+// f1.identify()
+
+b1.all();
